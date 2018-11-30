3c0aae11c17813002bfd3e9eb2ab3ca430861301
GreyFerret <s.k.minaev@gmail.com>
2018-11-30 14:09:18 +0300

    Changes to prize pool

--- src/main/java/it/greyferret/ferretbot/entity/PrizeDefault.java
+++ src/main/java/it/greyferret/ferretbot/entity/PrizeDefault.java
@@ -17,24 +17,23 @@ public class PrizeDefault {
 		prizes.add(new Prize("300 IQ", 3, type));
 		prizes.add(new Prize("500 IQ", 2, type));
 		prizes.add(new Prize("1000 IQ", 1, type));
-		prizePool = new PrizePool(type, prizes, 0.055);
+		prizePool = new PrizePool(type, prizes, 0.041);
 		res.put(type, prizePool);
 
 		type = 1;
 		prizes = new ArrayList<>();
 		prizes.add(new Prize("Случайное простое желание", 8, type));
 		prizes.add(new Prize("Случайное сложное желание", 4, type));
-		prizes.add(new Prize("Сыграть дуэтом", 2, type));
+		prizes.add(new Prize("Первый Spec-час", 2, type));
 		prizes.add(new Prize("Заказной стрим на 2 часа", 1, type));
-		prizePool = new PrizePool(type, prizes, 0.318);
+		prizePool = new PrizePool(type, prizes, 0.233);
 		res.put(type, prizePool);
 
 		type = 2;
 		prizes = new ArrayList<>();
 		prizes.add(new Prize("Платная подписка на канал", 2, type));
-		prizes.add(new Prize("Маунт для BRR", 4, type));
 		prizes.add(new Prize("500 руб.", 2, type));
-		prizePool = new PrizePool(type, prizes, 0.096);
+		prizePool = new PrizePool(type, prizes, 0.019);
 		res.put(type, prizePool);
 
 		return res;
