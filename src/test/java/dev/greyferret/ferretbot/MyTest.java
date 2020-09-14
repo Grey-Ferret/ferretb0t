8b4d2e473e0bb8c2c997d54921b75d04e607010e
Minaev Sergey <s.k.minaev@gmail.com>
2020-09-14 04:26:35 +0300

    test

---
+++ src/test/java/dev/greyferret/ferretbot/MyTest.java
@@ -0,0 +1,15 @@
+package dev.greyferret.ferretbot;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.fail;
+
+@SpringBootTest
+public class MyTest {
+	@Test
+	public void test() {
+		fail();
+	}
+}
